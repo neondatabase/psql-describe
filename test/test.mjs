b871f7832940f996b3c5e35486673de361cecf9b
George MacKerron <george@mackerron.co.uk>
2023-11-14 13:00:13 +0100

    Refactoring to eliminate globals and use http transport

--- test/test.mjs
+++ test/test.mjs
@@ -5,8 +5,14 @@ import fs from 'fs';
 import { describe, describeDataToString } from '../src/describe.mjs';
 import { spawnSync } from 'child_process';
 
-const [psqlPath, dbUrl] = process.argv.slice(2);
-console.log(`PSQL: ${psqlPath}\nDB:   ${dbUrl.replace(/(?<=^postgres(ql)?:[/][/][^:]+:)[^@]+(?=@)/, '***')}\n`);
+const [psqlPath, dbUrl, transport] = process.argv.slice(2);
+const useHttp = transport === 'http';
+
+console.log(`
+PSQL:      ${psqlPath}
+DB:        ${dbUrl.replace(/(?<=^postgres(ql)?:[/][/][^:]+:)[^@]+(?=@)/, '***')}
+Transport: ${useHttp ? 'http' : 'pg'}
+`);
 
 function psql(input) {
   const { stdout, stderr } = spawnSync(psqlPath, [dbUrl, '-E'], {
@@ -22,12 +28,37 @@ function countLines(str) {
   return lineCount;
 }
 
-const
+let queryFn, pool;
+
+if (useHttp) {
+  const headers = {
+    'Neon-Connection-String': dbUrl,
+    'Neon-Raw-Text-Output': 'true',  // because we want raw Postgres text format
+    'Neon-Array-Mode': 'true',  // this saves data and post-processing even if we return objects, not arrays
+    'Neon-Pool-Opt-In': 'true',
+  };
+  queryFn = async (sql) => {
+    const response = await fetch('https://proxy.localtest.me:4444/sql', {
+      method: 'POST',
+      body: JSON.stringify({ query: sql, params: [] }),
+      headers,
+    });
+    const json = await response.json();
+    if (response.status === 200) return json;
+    const msg = json.message.match(/ERROR: (.*?)\n/)[1];
+    throw new Error(msg);
+  }
+
+} else {
+  pg.types.getTypeParser = () => x => x;  // raw pg text format for everything
   pool = new pg.Pool({
     connectionString: dbUrl,
     application_name: 'psql',  // to match psql for /dconfig
-  }),
-  queryFn = sql => pool.query({ text: sql, rowMode: 'array' }),
+  });
+  queryFn = sql => pool.query({ text: sql, rowMode: 'array' })
+}
+
+const
   testsStr = fs.readFileSync(process.stdin.fd, 'utf-8'),
   tests = testsStr.split('\n').map(t => t.trim()).filter(x => !!x);
 
@@ -35,16 +66,27 @@ for (let test of tests) {
   const psqlOutput = psql(test);
 
   const localOutputArr = [];
-  await describe(pg, test, 'main', queryFn, x => localOutputArr.push(x), true);
+  await describe(test, 'main', queryFn, x => localOutputArr.push(x), true).promise;
   const localOutput = localOutputArr.map(x => describeDataToString(x)).join('\n\n');
 
-  const stdPsqlOutput = psqlOutput.replace(/ +$/gm, '').trim();  // .replace(/\n\(\d+ rows?\)/g, '')
+  const stdPsqlOutput = psqlOutput.replace(/ +$/gm, '').trim();
   const stdLocalOutput = localOutput.replace(/ +$/gm, '').trim();
 
   let pass = stdPsqlOutput === stdLocalOutput;
 
-  // weird exception
-  if (!pass && test === '\\dFp+' && stdPsqlOutput.replace(/\n\(\d+ rows?\)/g, '') === stdLocalOutput.replace(/\n\(\d+ rows?\)/g, '')) pass = true;
+  // exceptions: ignore unimportant differences
+  if (!pass) {
+    if (test === '\\dFp+' &&
+      stdPsqlOutput.replace(/\n\(\d+ rows?\)/g, '')
+      === stdLocalOutput.replace(/\n\(\d+ rows?\)/g, '')) {
+      pass = true;
+
+    } else if ((test === '\\dconfig' || test === '\\dconfig+') &&
+      stdPsqlOutput.replace(/\n\s*application_name[^\n]+/, '').replace(/\n\(\d+ rows?\)/g, '')
+      === stdLocalOutput.replace(/\n\(\d+ rows?\)/g, '')) {
+      pass = true;
+    }
+  }
 
   const lineCount = countLines(stdLocalOutput);
   console.log(pass ? 'Pass' + ' '.repeat(6 - String(lineCount).length) : '*** FAIL ***', lineCount, ' ' + test);
@@ -56,4 +98,4 @@ for (let test of tests) {
   }
 }
 
-await pool.end();
+if (!useHttp) await pool.end();
